44e4b2e7807c9eb80d93ee7d2bfa8dbc65a8de39
Windzor De Guzman <wdavedeguzman@gmail.com>
2020-11-30 14:12:51 -0800

    student seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
         	BorrowedEquipmentsTableSeeder::class,
-        	EquipmentsTableSeeder::class
+        	EquipmentsTableSeeder::class,
+            StudentsTableSeeder::class
         ]);
     }
 }
