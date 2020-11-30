0ab7847e10a20d3827cfc74f2a8fc09b73357639
Windzor De Guzman <wdavedeguzman@gmail.com>
2020-11-30 11:29:17 -0800

    seeds

---
+++ database/factories/borrowed_equipmentFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\borrowed_equipment;
+use Faker\Generator as Faker;
+
+$factory->define(borrowed_equipment::class, function (Faker $faker) {
+    return [
+        'student_id' => mt_rand(1,3), 
+        'equipment_id' => mt_rand(1,4), 
+        'number_of_item' => mt_rand(0,15),
+        'rent_start_date' => $faker->date(),
+        'rent_end_date' => $faker->date(),
+        'facility' => $faker->company(),
+        'facilitator' => $faker->userName()
+    ];
+});
+
---
+++ database/seeds/BorrowedEquipmentsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BorrowedEquipmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    	$borrowed_equipments = factory(\App\borrowed_equipment::class, 15)->create();
+
+    }
+}
