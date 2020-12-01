723f4b1253dbea35ccb55e84f342e1e26c9b3e77
MamaVen <46667060+MamaVen@users.noreply.github.com>
2020-12-01 05:46:15 -0800

    Add files via upload

---
+++ EquipmentsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EquipmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $equipments = [
+            ['name' => 'Basketball', 'model' => 'GX7'],
+            ['name' => 'Volleyball', 'model' => 'GX897'],
+            ['name' => 'Softball', 'model' => 'GX57'],
+            ['name' => 'Karate Mat', 'model' => 'BlueOne']
+
+        ];
+
+        DB::table('equipment')->insert($equipments);
+    }
+}
