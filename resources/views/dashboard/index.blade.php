185f6548e93b115e6c79fecd69f7f4e180d26ec3
Windzor De Guzman <wdavedeguzman@gmail.com>
2020-12-03 23:17:08 -0800

    footer

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,8 +5,16 @@
     	<br>
     	<br>
 		<h3 class="center">
-			<h1><center>Welcome {{ auth()->user()->name }}</center></h1>
+			<h1 class="welcome"><center>Welcome {{ auth()->user()->name }}</center></h1>
 		</h3>
     </div>
 
+
+
+<style type="text/css">
+	
+ .welcome { 
+ 	font-size: 50px;
+  }
+</style>
 @endsection
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,20 +1,80 @@
- <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.footer {
-   position: fixed;
-   left: 0;
-   bottom: 0;
-   width: 100%;
-   padding-top: 1%;
-   background-color: 	#7f948a;
-   color: black;
-   text-align: center;
-}
-</style>
-</head>
-<body>
+<br>
+<br>
+<br>
+<br>
 
 
-<div class="footer">
-  <p>© 2020 Copyright </p>
+<footer class="mainfooter" role="contentinfo">
+	<div class="footer-middle">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-sm-6">
+					<!--Column1-->
+					<div class="footer-pad">
+						<h4>Needs</h4>
+						<ul class="list-unstyled">
+							<li><a href="#">Books</a></li>
+							<li><a href="#">Equipments</a></li>
+							<li><a href="#">Devices</a></li>
+							<li><a href="#">Lockers</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<!--Column1-->
+					<div class="footer-pad">
+						<h4>Borrowed Sections</h4>
+						<ul class="list-unstyled">
+							<li><a href="#">Borrowed Devices</a></li>
+							<li><a href="#">Borrowed Books</a></li>
+							<li><a href="#">Borrowed Equipments</a></li>
+							<li><a href="#">Locker Renting</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<!--Column1-->
+					<div class="footer-pad">
+						<p>We are a group of students taking BSIT. A Borrowing and Renting System created by Group 4 - 3C</p>
+
+					</div>
+				</div>
+			</div>
+
+
+	</div>
 </div>
+</footer>
+
+<style type="text/css">
+	
+	/*FOOTER*/
+
+	footer {
+		background: #16222A;
+		background: linear-gradient(59deg, #3A6073, #16222A);
+		color: white;
+		margin-top:100px;
+	}
+
+	footer a {
+		color: #fff;
+		font-size: 14px;
+		transition-duration: 0.2s;
+	}
+
+	footer a:hover {
+		color: #FA944B;
+		text-decoration: none;
+	}
+
+	
+
+	.footer-middle {
+		padding-top: 2em;
+		color: white;
+	}
+
+
+
+</style>
